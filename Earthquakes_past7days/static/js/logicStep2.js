f43f23e565e801a8f1f64bd6df6188f91818b9fa
kchavez05 <93493741+kchavez05@users.noreply.github.com>
2022-03-21 09:46:41 -0600

    Added magnitude circle markers

--- Earthquakes_past7days/static/js/logicStep2.js
+++ Earthquakes_past7days/static/js/logicStep2.js
@@ -34,22 +34,46 @@ L.control.layers(baseMaps).addTo(map);
 // Accessing the Toronto airline routes GeoJSON URL.
 let earthquakeData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-// Create a style for the lines.
-let myStyle = {
-  color: "blue",
-  weight: 1,
-  fillColor: "#FFFF00"
-};
+
+
 
 //Grabbing our GeoJSON data.
 d3.json(earthquakeData).then(function(data) {
   console.log(data);
+  // This function returns the style data for each of the earthquakes we plot on
+    // the map. We pass the magnitude of the earthquake into a function
+    // to calculate the radius.
+  function styleInfo(feature) {
+    return {
+        opacity: 1,
+        fillOpactiy: 1,
+        fillColor: "#ffae42",
+        color: "#000000",
+        radius: getRadius(feature.properties.mag),
+        stroke: true,
+        weight: 0.5
+    };
+
+}
+    // This function determines the radius of the earthquake marker based on its magnitude.
+    // Earthquakes with a magnitude of 0 will be plotted with a radius of 1.  
+    function getRadius(magnitude) {
+        if (magnitude === 0) {
+            return 1;
+        }
+        return magnitude *4;
+    };
+
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJSON(data, {
-        // style: myStyle,
-        onEachFeature: function(feature, layer) {
-      console.log(layer);
-      layer.bindPopup("<h2>" + feature.properties.mag + "</h2> <hr> <h3>" + feature.properties.place + "</h3>");
-    }})
-    .addTo(map)
-  });
+        style: styleInfo,
+        pointToLayer: function(feature, latlng) {
+            console.log(data);
+            return L.circleMarker(latlng);
+        }
+    //   console.log(layer);
+    //   layer.bindPopup("<h2>" + feature.properties.mag + "</h2> <hr> <h3>" + feature.properties.place + "</h3>");
+    // }})
+    
+  }).addTo(map);
+});
